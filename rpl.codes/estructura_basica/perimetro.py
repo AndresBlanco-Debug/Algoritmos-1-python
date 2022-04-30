5bbf08b80fea7ee6bf97df96fc5ad809d42730c5
Jose Blanco <85774760+AndresBlanco-Debug@users.noreply.github.com>
2022-04-29 21:48:54 -0300

    Create perimetro.py

---
+++ rpl.codes/estructura_basica/perimetro.py
@@ -0,0 +1,12 @@
+"""Escribir un programa que solicite al usuario que ingrese su edad y que imprima por pantalla si el usuario es mayor de edad o no.
+
+Ejemplos:
+
+    >>> Ingrese edad: 18
+    Es mayor de edad
+    >>> Ingrese edad: 17
+    Es menor de edad"""
+
+base=int(input("Ingrese la base del rectangulo: "))
+altura = int(input("Ingrese la altura del rectangulo: "))
+print("El perimetro del rectangulo es:",(2*base + 2*altura))
